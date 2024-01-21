c9c73a43ca25a3958405a78ef487c1cd490f649f
yichehnc <yichenc2017@gmail.com>
2024-01-21 23:45:48 +1100

    feat: update header file

--- sort.h
+++ sort.h
@@ -1,12 +1,9 @@
-#ifndef _SORT_H_
-#define _SORT_H_
+#ifndef SORT_H
+#define SORT_H
 
-#include <stdio.h>
-#include <unistd.h>
-#include <errno.h>
-#include <string.h>
 #include <stdlib.h>
-#include <ctype.h>
+#include <stdio.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -23,6 +20,12 @@ typedef struct listint_s
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+
+void swap(int *a, int *b);
+listint_t *create_listint(const int *array, size_t size);
+void solve(int *array, int start, int stop, size_t size);
+int partition(int arr[], int low, int high, size_t size);
+
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
